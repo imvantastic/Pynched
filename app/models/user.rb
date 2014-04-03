c7b61997ecdcb5bb3ff217a3bb3ad44d47bf8bbb
Van Nguyen <vanleslienguyen@gmail.com>
2014-04-03 00:29:17 -0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+	#creating a user
 	before_save { self.email = email.downcase }
 	validates(:firstName, presence: true, length: { maximum: 50 })
 	validates(:lastName, presence: true, length: { maximum: 50 })
@@ -7,4 +9,21 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+	
+	#creating a session
+	before_create :create_remember_token
+	
+	def User.new_remember_token
+       SecureRandom.urlsafe_base64
+    end
+
+    def User.hash(token)
+       Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+
+       def create_remember_token
+          self.remember_token = User.hash(User.new_remember_token)
+       end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401075629) do
+ActiveRecord::Schema.define(version: 20140403060509) do
 
   create_table "users", force: true do |t|
     t.string   "firstName"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20140401075629) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
