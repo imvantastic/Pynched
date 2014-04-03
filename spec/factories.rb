838ec04b0640be1c5349b69914c95ccfe034fc40
Van Nguyen <vanleslienguyen@gmail.com>
2014-04-02 20:21:36 -0700

    finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    firstName     "Van"
+	lastName "Nguyen"
+    email    "vanleslienguyen@gmail.com"
+    password "Vnguy3n"
+    password_confirmation "Vnguy3n"
+  end
+end
